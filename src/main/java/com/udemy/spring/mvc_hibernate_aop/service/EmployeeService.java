8f9e0b774875e16d2f97ee4f0434bfb835697f97
p_Ilya <petermoloch@mail.ru>
2022-08-08 20:28:29 +0300

    @Service

---
+++ src/main/java/com/udemy/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.udemy.spring.mvc_hibernate_aop.service;
+
+import com.udemy.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+}
